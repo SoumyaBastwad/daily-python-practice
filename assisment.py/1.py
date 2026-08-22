e77642d3171797692b8988825513f5dfaa639b45
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-22 22:14:54 +0530

    Update 1.py

--- assisment.py/1.py
+++ assisment.py/1.py
@@ -1,6 +1,15 @@
 # write a program to find maximum and minimum elements in array
 # sample input:[5,3,9,2,8]
-
+arr=[5,3,9,2,8]
+max=arr[0]
+min=arr[0]
+for i in arr:
+    if i>max:
+        max=i
+    if i<min:
+        min=i
+print("Maximum",max)
+print("Minimum",min)
 
 
 
