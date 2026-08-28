83fc809087147a1d8d21c07c7744fd50eed92bd2
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-28 21:29:00 +0530

    Update ExceptionHandling.py

--- DAY--5/ExceptionHandling.py
+++ DAY--5/ExceptionHandling.py
@@ -121,38 +121,3 @@ else:
     print("your amount has withdrawal")
 
 
-# write program  to manu driven code
-import sys
-def addition():
-    a=int(input("enter a value:"))
-    b=int(input("enter b value:"))
-    print("Addition=",a+b)
-def substraction():
-    a=int(input("enter a value:"))
-    b=int(input("enter b value:"))
-    print("Substraction=",a-b)
-def multiplication():
-    a=int(input("enter a value:"))
-    b=int(input("enter b value:"))
-    print("Multiplication=",a*b)
-def Division():
-    a=int(input("enter a value:"))
-    b=int(input("enter b value:"))
-    print("Division=",a/b)
-while True:
-    print("1.Addition:")
-    print("2.Substraction:")
-    print("3.Multiplication:")
-    print("4.Division:")
-    print("5.exit")
-    choice=int(input("enter your choice:"))
-    if choice==1:
-        addition()
-    elif choice==2:
-        substraction()
-    elif choice==3:
-        multiplication()
-    elif choice==4:
-        Division()
-    elif choice==5:
-        sys.exit()
