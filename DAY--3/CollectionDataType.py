137857a9ebf8962b32632912f2683eaa115c1444
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-28 21:27:15 +0530

    Update CollectionDataType.py

--- DAY--3/CollectionDataType.py
+++ DAY--3/CollectionDataType.py
@@ -242,6 +242,12 @@ yourset={10,50,60,30}
 print(myset.difference(yourset))
 
 
+# clear() we can use to clear data
+# pop() method is used to remove object
+myset={10,20,30,40}
+print(myset.clear()) #None
+
+
 
 
 
