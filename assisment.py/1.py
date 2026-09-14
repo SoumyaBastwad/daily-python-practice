798dc7ebd08ac8061e29a4178632bd4f1e99f306
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-14 19:44:08 +0530

    Update 1.py

--- assisment.py/1.py
+++ assisment.py/1.py
@@ -3,13 +3,7 @@
 arr=[5,3,9,2,8]
 max=arr[0]
 min=arr[0]
-for i in arr:
-    if i>max:
-        max=i
-    if i<min:
-        min=i
-print("Maximum",max)
-print("Minimum",min)
+
 
 # write a program to reverse the order of element in array
 # sample input:[1,2,3,4,5]
