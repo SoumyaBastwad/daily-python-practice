52546d06c974742351c2b9003c1f8c0505c841fd
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-14 19:46:34 +0530

    Update 1.py

--- assisment.py/1.py
+++ assisment.py/1.py
@@ -65,10 +65,4 @@ for key in dict:
 
 # write a program to check if a list is palindrome or not
 str="racecar"
-rev=""
-for i in str:
-     rev=i+rev
-if rev==str:
-     print("palindrome")
-else:
-     print("not palindrome")
+rev=
